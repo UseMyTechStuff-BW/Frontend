b13c322153337b35250c7f1ef8301d827cadf435
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-06-23 18:31:58 -0500

    Adds forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import AddEquipmentForm from "./components/AddEquipmentForm";
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
+    <>
+    <header>
     <div className="logoAndHeading">
-      <img
+      <Link to='/'><img
       className="logo" 
       src="https://dcassetcdn.com/design_img/501499/151249/151249_3571586_501499_thumbnail.jpg"
       alt="tech stuff"
-      />
+      /></Link>
       <h1 className="mainHeading">Use My Tech Stuff</h1>
     </div>
+    <nav>
+      <Link to='/login'>Login</Link>
+      <Link to='/register'>Register</Link>
+    </nav>
+    </header>
+
+    </>
   );
 }
 
